8620c5bf13ad88f621e41c1d8e89fd52b135e646
Axel Howind <axel@dua3.com>
2018-01-23 22:14:43 +0100

    init

---
+++ src/main/java/com/dua3/fxbrowser/FxBrowser.java
@@ -0,0 +1,25 @@
+package com.dua3.fxbrowser;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxBrowser extends Application {
+
+    private static final Logger LOG = LogManager.getLogger(FxBrowser.class);
+
+    static final String APP_NAME = "FxBrowser";
+
+    public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
