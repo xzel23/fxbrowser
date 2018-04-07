c2f82e2eb3eae02d79ac57c405bd7330a9cc1cb2
Axel Howind <axel@dua3.com>
2018-04-07 09:17:05 +0200

    start work on modularised build

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * @author axel
+ *
+ */
+module com.dua3.fxbrowser {
+	exports com.dua3.fxbrowser;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.web;
+	
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+}
